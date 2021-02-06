10277109302cb074ed1dc0d36be9cf77dc6d0146
Silvia Taliana <silviataliana2@gmail.com>
2021-02-06 19:18:55 +1030

    fixed uv index

--- script.js
+++ script.js
@@ -54,11 +54,12 @@ searchButton.on("click", function (event) {
         var lon = response.coord.lon;
         var lat = response.coord.lat;
 
-        var UVRating = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=a0eab1d4c4a0b9a91854301eece7ccae"
+        var UVRating = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=a0eab1d4c4a0b9a91854301eece7ccae"
         $.ajax({
             url: UVRating,
             method: "GET"
         }).then(function (response) {
+            console.log(response);
             var uvIndex = $("<p>");
             var uvSpan = $("<span>");
             uvIndex.text("UV Index: ");
@@ -99,13 +100,13 @@ searchButton.on("click", function (event) {
                 let hour = moment(response.list[i].dt_txt);
                 if (hour.hour() == 12) {
                     let date = $("<h3>");
-                    date.text(moment(response.list[i].dt_txt).format('MMMM Do YYYY'));
+                    date.text(moment(response.list[i].dt_txt).format('MMM Do YYYY'));
                     $(box).append(date);
 
                     // getting icon
                     var icon = $("<img />").attr({
                         "id": "icon",
-                        "src": "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png",
+                        "src": "https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png",
                     });
                     $(box).append(icon);
 
@@ -146,17 +147,3 @@ function displayButtons() {
         // console.log(cityList[i]);
     }
 };
-
-
-// pseudo code:
-// 1. get data from api using ajax
-// 2. THEN create a function to bring up response
-// 3. identify and call upon required data to be displayed on the screen
-// i.e. city name, date, icon representation of weather conditions, temperature,
-// humidity, wind speed and UV index (colour coded for favourable/moderate/severe)
-// 4. display 5 day forecast with date, icon representation of weather conditions,
-// temperature and humidity
-// 5. create a list and store searched city in there
-// 6. make that list a button so the city can be searched again
-// 7. store searches in local storage and redisplay to screen upon refreshing
-// if time, create toggle buttons for different units
